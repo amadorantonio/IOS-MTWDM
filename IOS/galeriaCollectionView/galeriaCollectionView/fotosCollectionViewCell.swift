75c2782844224ce33c0d4affd5e62cf439d04955
unknown <amador.barajas.antonio@gmail.com>
2021-04-24 21:47:46 -0500

    Se agregarn practicas realizadas en la sesion 12

---
+++ IOS/galeriaCollectionView/galeriaCollectionView/fotosCollectionViewCell.swift
@@ -0,0 +1,13 @@
+//
+//  fotosCollectionViewCell.swift
+//  galeriaCollectionView
+//
+//  Created by MTWDM on 24/04/21.
+//
+
+import UIKit
+
+class fotosCollectionViewCell: UICollectionViewCell {
+    @IBOutlet weak var foto: UIImageView!
+    
+}
---
+++ IOS/galeriaJsonCollectionView/galeriaJsonCollectionView/Fotografia.swift
@@ -0,0 +1,14 @@
+//
+//  Fotografia.swift
+//  galeriaJsonCollectionView
+//
+//  Created by MTWDM on 24/04/21.
+//
+
+import Foundation
+
+
+struct Fotografia:Decodable {
+    var id:String
+    var urls:[String:String]
+}
